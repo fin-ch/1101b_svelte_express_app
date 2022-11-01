bc559034ebcf2b61f73814567f50a3e6df41d594
fin-ch <woody101@snu.ac.kr>
2022-11-01 22:15:23 +0900

    ..

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import handler from "./.svelte/build/handler.js";
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,9 @@ const port = 3000;
 app.get("/expressroute", (req, res) => {
     res.send("This is an expess route!");
 });
+
+// THis wilㅣ connect your svelte kit app to your express server
+app.use(handler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
